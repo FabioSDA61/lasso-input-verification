b9c75226592c24cc20175158adffc2a4bd1c329b
FabioSDA61 <fabio.schasse@gmail.com>
2021-12-20 21:33:33 +0100

    test

--- test.js
+++ test.js
@@ -1,8 +1,27 @@
-axios
- .get("https://app.verify-email.org/api/v1/DwmTd6FIsAGDKe7GMi1YpSSESwoLuzO0uG8VOC3NLki2LVzsD5/verify/support@verify-email.org")
- .then(function(response) {
-  console.log(response);
- })
- .catch(function(error) {
-  console.log(error);
- });
+function testThatEmail () {
+    let email = document.getElementById('email')
+
+    axios
+    .get("https://app.verify-email.org/api/v1/DwmTd6FIsAGDKe7GMi1YpSSESwoLuzO0uG8VOC3NLki2LVzsD5/verify/"+email+"")
+    .then(function(response) {
+        if (response.data.status == 1) {
+            console.log("yay")
+        } else if (response.data.status == 0) {
+            console.log("nay")
+        } else {
+            console.log("neither")
+        }
+
+    console.log(response);
+    })
+    .catch(function(error) {
+    console.log(error);
+    });
+
+}
+
+document.getElementById('verify').setAttribute("onclick","testThatEmail()");
+
+
+
+

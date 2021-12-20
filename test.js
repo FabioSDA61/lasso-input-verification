e8a9045c21aa58130412d05c9f2906a8eba542a1
FabioSDA61 <fabio.schasse@gmail.com>
2021-12-20 21:22:30 +0100

    test

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-
-console.log("test 234")
+axios
+ .get("https://app.verify-email.org/api/v1/DwmTd6FIsAGDKe7GMi1YpSSESwoLuzO0uG8VOC3NLki2LVzsD5/verify/support@verify-email.org")
+ .then(function(response) {
+  console.log(response);
+ })
+ .catch(function(error) {
+  console.log(error);
+ });

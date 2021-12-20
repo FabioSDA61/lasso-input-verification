b5cfbafde68dc43471a4f507cc3075ac73e92fc1
FabioSDA61 <fabio.schasse@gmail.com>
2021-12-20 21:56:30 +0100

    test

--- test.js
+++ test.js
@@ -9,8 +9,11 @@ document.getElementById('verify').onclick = function () {
     .then(function(response) {
         if (response.data.status == 1) {
             console.log("yay")
+            document.getElementById('submit').style.display = "block"
+            document.getElementById('pass').style.display = "block"
         } else {
-            console.log("neither")
+            console.log("nay")
+            document.getElementById('fail').style.display = "block"
         }
 
     console.log(response);

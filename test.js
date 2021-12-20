d7dc45264367efb4d77afa619b99e2fa1dfd56da
FabioSDA61 <fabio.schasse@gmail.com>
2021-12-20 21:43:23 +0100

    test

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
-function testThatEmail () {
-    let email = document.getElementById('email')
+
+
+document.getElementById('verify').onclick = function () {
+    console.log("onClick")
+    let email = document.getElementById('email').value
 
     axios
     .get("https://app.verify-email.org/api/v1/DwmTd6FIsAGDKe7GMi1YpSSESwoLuzO0uG8VOC3NLki2LVzsD5/verify/"+email+"")
     .then(function(response) {
         if (response.data.status == 1) {
             console.log("yay")
-        } else if (response.data.status == 0) {
-            console.log("nay")
         } else {
             console.log("neither")
         }
@@ -20,7 +21,6 @@ function testThatEmail () {
 
 }
 
-document.getElementById('verify').setAttribute("onclick","testThatEmail()");
 
 
 
